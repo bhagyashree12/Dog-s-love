4c3d86933183a7d89e9f080c59f78afb16b7de0b
bhagyashree12 <bhagyashreesarode15@gmail.com>
2019-07-02 20:04:41 +0530

    Dog'S love

---
+++ app/src/main/java/com/example/myfirstapp_challege_1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myfirstapp_challege_1;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+
+}
